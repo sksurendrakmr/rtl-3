14ece03d139f10554cadda3ab3268a867ab5238a
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-07 00:56:29 +0530

    src/components/pets/__tests__/Pets.test.tsx

--- src/components/pets/__tests__/Pets.test.tsx
+++ src/components/pets/__tests__/Pets.test.tsx
@@ -1,6 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { Pets } from "../Pets";
-
+/**
+ * This test really making an HTTP request to our server to get the data
+ * even though our element is in virtual dom.
+ *
+ * Thus, This test will dependent on our server working correctly and returing
+ * the same data.
+ * E.g if server error occurs, our test will also going to fail.
+ *
+ * If server return different data then also our test is going to fail.
+ *
+ * Calling our server cost money.
+ *
+ * Calling server is slow (might take times to get the response).
+ */
 describe("pets", () => {
   test("should render mocked petCard", async () => {
     render(<Pets />);
